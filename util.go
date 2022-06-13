b1b67c9270daacf752863e5a314ed17fc7cec5c9
奇葩の灵梦 <415551921@qq.com>
2022-06-13 14:21:30 +0800

    optimize

--- util.go
+++ util.go
@@ -61,35 +61,51 @@ func Search(img1 image.Image, x int, y int, img2 image.Image) (point image.Point
 	return image.Point{}, false
 }
 
+type screenImage struct {
+	bounds image.Rectangle
+	imgs   []image.Image
+}
+
+func (s *screenImage) ColorModel() color.Model {
+	return s.imgs[0].ColorModel()
+}
+
+func (s *screenImage) Bounds() image.Rectangle {
+	return s.bounds
+}
+
+func (s *screenImage) At(x, y int) color.Color {
+	var r0, g0, b0, a0 uint32
+	for _, img := range s.imgs {
+		r, g, b, a := img.At(x, y).RGBA()
+		r0 = 65535 - (65535-r)*(65535-r0)/65535
+		g0 = 65535 - (65535-g)*(65535-g0)/65535
+		b0 = 65535 - (65535-b)*(65535-b0)/65535
+		a0 = 65535 - (65535-a)*(65535-a0)/65535
+	}
+	return color.RGBA{R: uint8(r0 >> 8), G: uint8(g0 >> 8), B: uint8(b0 >> 8), A: uint8(a0 >> 8)}
+}
+
 // Screen 滤色
-func Screen(imgs ...image.Image) *image.RGBA {
-	maxSize := imgs[0].Bounds()
+func Screen(imgs ...image.Image) image.Image {
+	if len(imgs) == 0 {
+		panic("need at least 1 image")
+	}
+	rect := imgs[0].Bounds()
 	for i := 1; i < len(imgs); i++ {
 		bounds := imgs[i].Bounds()
-		maxSize.Min.X = max(maxSize.Min.X, bounds.Min.X)
-		maxSize.Min.Y = max(maxSize.Min.Y, bounds.Min.Y)
-		maxSize.Max.X = min(maxSize.Max.X, bounds.Max.X)
-		maxSize.Max.Y = min(maxSize.Max.Y, bounds.Max.Y)
+		rect.Min.X = max(rect.Min.X, bounds.Min.X)
+		rect.Min.Y = max(rect.Min.Y, bounds.Min.Y)
+		rect.Max.X = min(rect.Max.X, bounds.Max.X)
+		rect.Max.Y = min(rect.Max.Y, bounds.Max.Y)
 	}
-	img0 := image.NewRGBA(image.Rect(maxSize.Min.X, maxSize.Min.Y, maxSize.Max.X, maxSize.Max.Y))
-	x0 := maxSize.Min.X
-	x1 := maxSize.Max.X
-	y0 := maxSize.Min.Y
-	y1 := maxSize.Max.Y
-	for x := x0; x < x1; x++ {
-		for y := y0; y < y1; y++ {
-			var r0, g0, b0, a0 uint32
-			for _, img := range imgs {
-				r, g, b, a := img.At(x, y).RGBA()
-				r0 = 65535 - (65535-r)*(65535-r0)/65535
-				g0 = 65535 - (65535-g)*(65535-g0)/65535
-				b0 = 65535 - (65535-b)*(65535-b0)/65535
-				a0 = 65535 - (65535-a)*(65535-a0)/65535
-			}
-			img0.SetRGBA(x, y, color.RGBA{R: uint8(r0 >> 8), G: uint8(g0 >> 8), B: uint8(b0 >> 8), A: uint8(a0 >> 8)})
-		}
+	if rect.Min.X >= rect.Max.X || rect.Min.Y >= rect.Max.Y {
+		panic("the images have no common area")
 	}
-	return img0
+	return &screenImage{
+		bounds: rect,
+		imgs:   imgs,
+	}
 }
 
 func min(a, b int) int {
@@ -106,6 +122,35 @@ func max(a, b int) int {
 	return b
 }
 
+type rotateImage struct {
+	img       image.Image
+	clockwise int8
+	rect      image.Rectangle
+}
+
+func (img *rotateImage) ColorModel() color.Model {
+	return img.img.ColorModel()
+}
+
+func (img *rotateImage) Bounds() image.Rectangle {
+	return img.rect
+}
+
+func (img *rotateImage) At(x, y int) color.Color {
+	switch img.clockwise {
+	case 0:
+		return img.img.At(x, y)
+	case 1:
+		return img.img.At(y, img.rect.Max.X-x-1)
+	case 2:
+		return img.img.At(img.rect.Max.X-x-1, img.rect.Max.Y-y-1)
+	case 3:
+		return img.img.At(img.rect.Max.Y-y-1, x)
+	}
+	panic("unreachable")
+}
+
+// Rotate 旋转图片，clockwise参数为1表示顺时针，2表示180°，-1表示逆时针
 func Rotate(img image.Image, clockwise int8) image.Image {
 	clockwise %= 4
 	if clockwise == 0 {
@@ -119,20 +164,9 @@ func Rotate(img image.Image, clockwise int8) image.Image {
 		rect.Min.X, rect.Min.Y = rect.Min.Y, rect.Min.X
 		rect.Max.X, rect.Max.Y = rect.Max.Y, rect.Max.X
 	}
-	img2 := image.NewRGBA(rect)
-	for i := rect.Min.X; i < rect.Max.X; i++ {
-		for j := rect.Min.Y; j < rect.Max.Y; j++ {
-			r, g, b, a := img.At(i, j).RGBA()
-			c := color.RGBA{R: uint8(r >> 8), G: uint8(g >> 8), B: uint8(b >> 8), A: uint8(a >> 8)}
-			switch clockwise {
-			case 1:
-				img2.SetRGBA(rect.Max.X-j-1, i, c)
-			case 2:
-				img2.SetRGBA(rect.Max.X-i-1, rect.Max.Y-j-1, c)
-			case 3:
-				img2.SetRGBA(j, rect.Max.Y-i-1, c)
-			}
-		}
+	return &rotateImage{
+		img:       img,
+		clockwise: clockwise,
+		rect:      rect,
 	}
-	return img2
 }
